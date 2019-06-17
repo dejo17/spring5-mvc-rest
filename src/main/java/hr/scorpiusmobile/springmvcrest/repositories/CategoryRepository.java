d6beb67772d99e7cea9b2b172bf2400fb896ba00
dejo_17 <dean.juricic@gmail.com>
2019-06-17 15:32:46 +0200

    initial commit + basic data model

---
+++ src/main/java/hr/scorpiusmobile/springmvcrest/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package hr.scorpiusmobile.springmvcrest.repositories;
+
+import hr.scorpiusmobile.springmvcrest.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
---
+++ src/test/java/hr/scorpiusmobile/springmvcrest/SpringmvcrestApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.scorpiusmobile.springmvcrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
