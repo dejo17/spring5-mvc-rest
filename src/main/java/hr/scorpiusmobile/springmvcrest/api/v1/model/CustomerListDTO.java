600880e0f3800414300f9432e499e7c8e2cd5211
dejo_17 <dean.juricic@gmail.com>
2019-06-18 09:35:54 +0200

    added DTOs for Customer

---
+++ src/main/java/hr/scorpiusmobile/springmvcrest/api/v1/model/CustomerListDTO.java
@@ -0,0 +1,11 @@
+package hr.scorpiusmobile.springmvcrest.api.v1.model;
+
+import hr.scorpiusmobile.springmvcrest.domain.Customer;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class CustomerListDTO {
+    private List<Customer> customers;
+}
