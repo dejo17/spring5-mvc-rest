e85d7d2cc0bf83a510c3c8f5bc99a0aee237e76e
dejo_17 <dean.juricic@gmail.com>
2019-06-18 11:07:59 +0200

    Added customers in data loader

--- src/main/java/hr/scorpiusmobile/springmvcrest/dataloader/DataLoader.java
+++ src/main/java/hr/scorpiusmobile/springmvcrest/dataloader/DataLoader.java
@@ -1,7 +1,9 @@
 package hr.scorpiusmobile.springmvcrest.dataloader;
 
 import hr.scorpiusmobile.springmvcrest.domain.Category;
+import hr.scorpiusmobile.springmvcrest.domain.Customer;
 import hr.scorpiusmobile.springmvcrest.repositories.CategoryRepository;
+import hr.scorpiusmobile.springmvcrest.repositories.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -9,9 +11,11 @@ import org.springframework.stereotype.Component;
 public class DataLoader implements CommandLineRunner {
 
     private CategoryRepository categoryRepository;
+    private CustomerRepository customerRepository;
 
-    public DataLoader(CategoryRepository categoryRepository) {
+    public DataLoader(CategoryRepository categoryRepository, CustomerRepository customerRepository) {
         this.categoryRepository = categoryRepository;
+        this.customerRepository = customerRepository;
     }
 
     @Override
@@ -39,7 +43,34 @@ public class DataLoader implements CommandLineRunner {
         categoryRepository.save(nuts);
 
 
-        System.out.println("Data Loaded = " + categoryRepository.count() );
+        System.out.println("Categories Loaded = " + categoryRepository.count());
 
+        Customer pero = new Customer();
+        pero.setLastName("Peric");
+        pero.setFirstName("Pero");
+
+        Customer dino = new Customer();
+        dino.setLastName("Dinic");
+        dino.setFirstName("Dino");
+
+        Customer alenko = new Customer();
+        alenko.setLastName("Haramija");
+        alenko.setFirstName("Alenko");
+
+        Customer dani = new Customer();
+        dani.setLastName("Glavic");
+        dani.setFirstName("Dani");
+
+        Customer djuro = new Customer();
+        djuro.setLastName("Djuric");
+        djuro.setFirstName("Djuro");
+
+        customerRepository.save(pero);
+        customerRepository.save(dino);
+        customerRepository.save(alenko);
+        customerRepository.save(dani);
+        customerRepository.save(djuro);
+
+        System.out.println("Customers Loaded = " + customerRepository.count());
     }
 }
